79619dd434eb95c43a8b1123a653d7ebc89ab2a9
tanay.kulshreshtha <tanay.kuls@gmail.com>
2023-03-13 09:44:57 +1100

    Cchanges

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -48,219 +48,262 @@ Partial Class Form1
         ' 
         ' btnBS
         ' 
-        btnBS.Font = New Font("Wingdings", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
-        btnBS.Location = New Point(35, 109)
+        btnBS.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnBS.Font = New Font("Wingdings", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnBS.ForeColor = SystemColors.AppWorkspace
+        btnBS.Location = New Point(69, 168)
         btnBS.Name = "btnBS"
-        btnBS.Size = New Size(68, 57)
+        btnBS.Size = New Size(73, 97)
         btnBS.TabIndex = 0
         btnBS.Text = "Õ"
-        btnBS.UseVisualStyleBackColor = True
+        btnBS.UseVisualStyleBackColor = False
         ' 
         ' btnPM
         ' 
-        btnPM.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btnPM.Location = New Point(257, 110)
+        btnPM.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnPM.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnPM.ForeColor = SystemColors.AppWorkspace
+        btnPM.Location = New Point(291, 169)
         btnPM.Name = "btnPM"
-        btnPM.Size = New Size(68, 57)
+        btnPM.Size = New Size(73, 97)
         btnPM.TabIndex = 0
         btnPM.Text = "±"
-        btnPM.UseVisualStyleBackColor = True
+        btnPM.UseVisualStyleBackColor = False
         ' 
         ' btnC
         ' 
-        btnC.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btnC.Location = New Point(183, 109)
+        btnC.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnC.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnC.ForeColor = SystemColors.AppWorkspace
+        btnC.Location = New Point(217, 168)
         btnC.Name = "btnC"
-        btnC.Size = New Size(68, 57)
+        btnC.Size = New Size(73, 97)
         btnC.TabIndex = 0
         btnC.Text = "C"
-        btnC.UseVisualStyleBackColor = True
+        btnC.UseVisualStyleBackColor = False
         ' 
         ' btnP
         ' 
-        btnP.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btnP.Location = New Point(257, 172)
+        btnP.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnP.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnP.ForeColor = SystemColors.AppWorkspace
+        btnP.Location = New Point(291, 231)
         btnP.Name = "btnP"
-        btnP.Size = New Size(68, 57)
+        btnP.Size = New Size(73, 97)
         btnP.TabIndex = 0
         btnP.Text = "+"
-        btnP.UseVisualStyleBackColor = True
+        btnP.UseVisualStyleBackColor = False
         ' 
         ' btn4
         ' 
-        btn4.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn4.Location = New Point(35, 235)
+        btn4.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn4.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn4.ForeColor = SystemColors.AppWorkspace
+        btn4.Location = New Point(69, 294)
         btn4.Name = "btn4"
-        btn4.Size = New Size(68, 57)
+        btn4.Size = New Size(73, 97)
         btn4.TabIndex = 0
         btn4.Text = "4"
-        btn4.UseVisualStyleBackColor = True
+        btn4.UseVisualStyleBackColor = False
         ' 
         ' btn5
         ' 
-        btn5.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn5.Location = New Point(109, 235)
+        btn5.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn5.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn5.ForeColor = SystemColors.AppWorkspace
+        btn5.Location = New Point(143, 294)
         btn5.Name = "btn5"
-        btn5.Size = New Size(68, 57)
+        btn5.Size = New Size(73, 97)
         btn5.TabIndex = 0
         btn5.Text = "5"
-        btn5.UseVisualStyleBackColor = True
+        btn5.UseVisualStyleBackColor = False
         ' 
         ' btn6
         ' 
-        btn6.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn6.Location = New Point(183, 235)
+        btn6.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn6.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn6.ForeColor = SystemColors.AppWorkspace
+        btn6.Location = New Point(217, 294)
         btn6.Name = "btn6"
-        btn6.Size = New Size(68, 57)
+        btn6.Size = New Size(73, 97)
         btn6.TabIndex = 0
         btn6.Text = "6"
-        btn6.UseVisualStyleBackColor = True
+        btn6.UseVisualStyleBackColor = False
         ' 
         ' btnM
         ' 
-        btnM.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btnM.Location = New Point(257, 235)
+        btnM.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnM.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnM.ForeColor = SystemColors.AppWorkspace
+        btnM.Location = New Point(291, 294)
         btnM.Name = "btnM"
-        btnM.Size = New Size(68, 57)
+        btnM.Size = New Size(73, 97)
         btnM.TabIndex = 0
         btnM.Text = "-"
-        btnM.UseVisualStyleBackColor = True
+        btnM.UseVisualStyleBackColor = False
         ' 
         ' btn7
         ' 
-        btn7.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn7.Location = New Point(35, 172)
+        btn7.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn7.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn7.ForeColor = SystemColors.AppWorkspace
+        btn7.Location = New Point(69, 231)
         btn7.Name = "btn7"
-        btn7.Size = New Size(68, 57)
+        btn7.Size = New Size(73, 97)
         btn7.TabIndex = 0
         btn7.Text = "7"
-        btn7.UseVisualStyleBackColor = True
+        btn7.UseVisualStyleBackColor = False
         ' 
         ' btn8
         ' 
-        btn8.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn8.Location = New Point(109, 172)
+        btn8.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn8.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn8.ForeColor = SystemColors.AppWorkspace
+        btn8.Location = New Point(143, 231)
         btn8.Name = "btn8"
-        btn8.Size = New Size(68, 57)
+        btn8.Size = New Size(73, 97)
         btn8.TabIndex = 0
         btn8.Text = "8"
-        btn8.UseVisualStyleBackColor = True
+        btn8.UseVisualStyleBackColor = False
         ' 
         ' btn9
         ' 
-        btn9.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn9.Location = New Point(183, 172)
+        btn9.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn9.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn9.ForeColor = SystemColors.AppWorkspace
+        btn9.Location = New Point(217, 231)
         btn9.Name = "btn9"
-        btn9.Size = New Size(68, 57)
+        btn9.Size = New Size(73, 97)
         btn9.TabIndex = 0
         btn9.Text = "9"
-        btn9.UseVisualStyleBackColor = True
+        btn9.UseVisualStyleBackColor = False
         ' 
         ' btnT
         ' 
-        btnT.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btnT.Location = New Point(257, 297)
+        btnT.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnT.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnT.ForeColor = SystemColors.AppWorkspace
+        btnT.Location = New Point(291, 356)
         btnT.Name = "btnT"
-        btnT.Size = New Size(68, 57)
+        btnT.Size = New Size(73, 97)
         btnT.TabIndex = 0
         btnT.Text = "×"
-        btnT.UseVisualStyleBackColor = True
+        btnT.UseVisualStyleBackColor = False
         ' 
         ' btn1
         ' 
-        btn1.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn1.Location = New Point(35, 297)
+        btn1.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn1.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn1.ForeColor = SystemColors.AppWorkspace
+        btn1.Location = New Point(69, 356)
         btn1.Name = "btn1"
-        btn1.Size = New Size(68, 57)
+        btn1.Size = New Size(73, 97)
         btn1.TabIndex = 0
         btn1.Text = "1"
-        btn1.UseVisualStyleBackColor = True
+        btn1.UseVisualStyleBackColor = False
         ' 
         ' btn2
         ' 
-        btn2.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn2.Location = New Point(109, 297)
+        btn2.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn2.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn2.ForeColor = SystemColors.AppWorkspace
+        btn2.Location = New Point(143, 356)
         btn2.Name = "btn2"
-        btn2.Size = New Size(68, 57)
+        btn2.Size = New Size(73, 97)
         btn2.TabIndex = 0
         btn2.Text = "2"
-        btn2.UseVisualStyleBackColor = True
+        btn2.UseVisualStyleBackColor = False
         ' 
         ' btn3
         ' 
-        btn3.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn3.Location = New Point(183, 297)
+        btn3.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn3.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn3.ForeColor = SystemColors.AppWorkspace
+        btn3.Location = New Point(217, 356)
         btn3.Name = "btn3"
-        btn3.Size = New Size(68, 57)
+        btn3.Size = New Size(73, 97)
         btn3.TabIndex = 0
         btn3.Text = "3"
-        btn3.UseVisualStyleBackColor = True
+        btn3.UseVisualStyleBackColor = False
         ' 
         ' btnD
         ' 
-        btnD.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btnD.Location = New Point(257, 360)
+        btnD.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnD.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnD.ForeColor = SystemColors.AppWorkspace
+        btnD.Location = New Point(291, 419)
         btnD.Name = "btnD"
-        btnD.Size = New Size(68, 57)
+        btnD.Size = New Size(73, 97)
         btnD.TabIndex = 0
         btnD.Text = "÷"
-        btnD.UseVisualStyleBackColor = True
+        btnD.UseVisualStyleBackColor = False
         ' 
         ' btnE
         ' 
-        btnE.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btnE.Location = New Point(183, 360)
+        btnE.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnE.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnE.ForeColor = SystemColors.AppWorkspace
+        btnE.Location = New Point(217, 419)
         btnE.Name = "btnE"
-        btnE.Size = New Size(68, 57)
+        btnE.Size = New Size(73, 97)
         btnE.TabIndex = 0
         btnE.Text = "="
-        btnE.UseVisualStyleBackColor = True
+        btnE.UseVisualStyleBackColor = False
         ' 
         ' txtDisplay
         ' 
-        txtDisplay.Font = New Font("Microsoft Sans Serif", 28.2F, FontStyle.Bold, GraphicsUnit.Point)
-        txtDisplay.Location = New Point(35, 33)
+        txtDisplay.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        txtDisplay.Font = New Font("Cascadia Mono", 19.875F, FontStyle.Bold, GraphicsUnit.Point)
+        txtDisplay.ForeColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        txtDisplay.Location = New Point(69, 94)
         txtDisplay.Name = "txtDisplay"
-        txtDisplay.Size = New Size(290, 61)
+        txtDisplay.Size = New Size(295, 69)
         txtDisplay.TabIndex = 1
         txtDisplay.Text = "0"
         txtDisplay.TextAlign = HorizontalAlignment.Right
         ' 
         ' btnCE
         ' 
-        btnCE.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btnCE.Location = New Point(109, 110)
+        btnCE.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnCE.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnCE.ForeColor = SystemColors.AppWorkspace
+        btnCE.Location = New Point(143, 167)
         btnCE.Name = "btnCE"
-        btnCE.Size = New Size(68, 57)
+        btnCE.Size = New Size(73, 97)
         btnCE.TabIndex = 0
         btnCE.Text = "CE"
-        btnCE.UseVisualStyleBackColor = True
+        btnCE.UseVisualStyleBackColor = False
         ' 
         ' btnDP
         ' 
-        btnDP.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btnDP.Location = New Point(109, 360)
+        btnDP.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btnDP.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btnDP.ForeColor = SystemColors.AppWorkspace
+        btnDP.Location = New Point(143, 419)
         btnDP.Name = "btnDP"
-        btnDP.Size = New Size(68, 57)
+        btnDP.Size = New Size(73, 97)
         btnDP.TabIndex = 0
         btnDP.Text = "."
-        btnDP.UseVisualStyleBackColor = True
+        btnDP.UseVisualStyleBackColor = False
         ' 
         ' btn0
         ' 
-        btn0.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
-        btn0.Location = New Point(35, 360)
+        btn0.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        btn0.Font = New Font("Cascadia Code", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        btn0.ForeColor = SystemColors.AppWorkspace
+        btn0.Location = New Point(69, 419)
         btn0.Name = "btn0"
-        btn0.Size = New Size(68, 57)
+        btn0.Size = New Size(73, 97)
         btn0.TabIndex = 0
         btn0.Text = "0"
-        btn0.UseVisualStyleBackColor = True
+        btn0.UseVisualStyleBackColor = False
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8.0F, 19.0F)
+        AutoScaleDimensions = New SizeF(14F, 31F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(362, 444)
+        BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(255))
+        ClientSize = New Size(444, 574)
         Controls.Add(txtDisplay)
         Controls.Add(btnE)
         Controls.Add(btnD)
@@ -282,7 +325,7 @@ Partial Class Form1
         Controls.Add(btn7)
         Controls.Add(btn4)
         Controls.Add(btnBS)
-        Font = New Font("Sylfaen", 9.0F, FontStyle.Regular, GraphicsUnit.Point)
+        Font = New Font("Sylfaen", 9F, FontStyle.Regular, GraphicsUnit.Point)
         Icon = CType(resources.GetObject("$this.Icon"), Icon)
         Name = "Form1"
         Text = "Calculator"
