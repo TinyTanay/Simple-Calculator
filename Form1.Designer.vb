bdef83032e246ae69bfda76b7fa374e656e447ad
Tiny <Tanay.kuls@gmail.com>
2023-03-13 08:12:34 +1100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,314 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnBS = New Button()
+        btnPM = New Button()
+        btnC = New Button()
+        btnP = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btnM = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btnT = New Button()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btnD = New Button()
+        btnE = New Button()
+        txtDisplay = New TextBox()
+        btnCE = New Button()
+        btnDP = New Button()
+        btn0 = New Button()
+        SuspendLayout()
+        ' 
+        ' btnBS
+        ' 
+        btnBS.Font = New Font("Wingdings", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        btnBS.Location = New Point(35, 109)
+        btnBS.Name = "btnBS"
+        btnBS.Size = New Size(68, 57)
+        btnBS.TabIndex = 0
+        btnBS.Text = "Õ"
+        btnBS.UseVisualStyleBackColor = True
+        ' 
+        ' btnPM
+        ' 
+        btnPM.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btnPM.Location = New Point(257, 110)
+        btnPM.Name = "btnPM"
+        btnPM.Size = New Size(68, 57)
+        btnPM.TabIndex = 0
+        btnPM.Text = "±"
+        btnPM.UseVisualStyleBackColor = True
+        ' 
+        ' btnC
+        ' 
+        btnC.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btnC.Location = New Point(183, 109)
+        btnC.Name = "btnC"
+        btnC.Size = New Size(68, 57)
+        btnC.TabIndex = 0
+        btnC.Text = "C"
+        btnC.UseVisualStyleBackColor = True
+        ' 
+        ' btnP
+        ' 
+        btnP.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btnP.Location = New Point(257, 172)
+        btnP.Name = "btnP"
+        btnP.Size = New Size(68, 57)
+        btnP.TabIndex = 0
+        btnP.Text = "+"
+        btnP.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn4.Location = New Point(35, 235)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(68, 57)
+        btn4.TabIndex = 0
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn5.Location = New Point(109, 235)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(68, 57)
+        btn5.TabIndex = 0
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn6.Location = New Point(183, 235)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(68, 57)
+        btn6.TabIndex = 0
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btnM
+        ' 
+        btnM.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btnM.Location = New Point(257, 235)
+        btnM.Name = "btnM"
+        btnM.Size = New Size(68, 57)
+        btnM.TabIndex = 0
+        btnM.Text = "-"
+        btnM.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn7.Location = New Point(35, 172)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(68, 57)
+        btn7.TabIndex = 0
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn8.Location = New Point(109, 172)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(68, 57)
+        btn8.TabIndex = 0
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn9.Location = New Point(183, 172)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(68, 57)
+        btn9.TabIndex = 0
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnT
+        ' 
+        btnT.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btnT.Location = New Point(257, 297)
+        btnT.Name = "btnT"
+        btnT.Size = New Size(68, 57)
+        btnT.TabIndex = 0
+        btnT.Text = "×"
+        btnT.UseVisualStyleBackColor = True
+        ' 
+        ' btn1
+        ' 
+        btn1.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn1.Location = New Point(35, 297)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(68, 57)
+        btn1.TabIndex = 0
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn2.Location = New Point(109, 297)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(68, 57)
+        btn2.TabIndex = 0
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn3.Location = New Point(183, 297)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(68, 57)
+        btn3.TabIndex = 0
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btnD
+        ' 
+        btnD.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btnD.Location = New Point(257, 360)
+        btnD.Name = "btnD"
+        btnD.Size = New Size(68, 57)
+        btnD.TabIndex = 0
+        btnD.Text = "÷"
+        btnD.UseVisualStyleBackColor = True
+        ' 
+        ' btnE
+        ' 
+        btnE.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btnE.Location = New Point(183, 360)
+        btnE.Name = "btnE"
+        btnE.Size = New Size(68, 57)
+        btnE.TabIndex = 0
+        btnE.Text = "="
+        btnE.UseVisualStyleBackColor = True
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.Font = New Font("Microsoft Sans Serif", 28.2F, FontStyle.Bold, GraphicsUnit.Point)
+        txtDisplay.Location = New Point(35, 33)
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.Size = New Size(290, 61)
+        txtDisplay.TabIndex = 1
+        txtDisplay.Text = "0"
+        txtDisplay.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' btnCE
+        ' 
+        btnCE.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btnCE.Location = New Point(109, 110)
+        btnCE.Name = "btnCE"
+        btnCE.Size = New Size(68, 57)
+        btnCE.TabIndex = 0
+        btnCE.Text = "CE"
+        btnCE.UseVisualStyleBackColor = True
+        ' 
+        ' btnDP
+        ' 
+        btnDP.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btnDP.Location = New Point(109, 360)
+        btnDP.Name = "btnDP"
+        btnDP.Size = New Size(68, 57)
+        btnDP.TabIndex = 0
+        btnDP.Text = "."
+        btnDP.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Font = New Font("Microsoft Sans Serif", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point)
+        btn0.Location = New Point(35, 360)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(68, 57)
+        btn0.TabIndex = 0
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 19.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(362, 444)
+        Controls.Add(txtDisplay)
+        Controls.Add(btnE)
+        Controls.Add(btnD)
+        Controls.Add(btnT)
+        Controls.Add(btnM)
+        Controls.Add(btnCE)
+        Controls.Add(btnP)
+        Controls.Add(btn3)
+        Controls.Add(btn9)
+        Controls.Add(btn6)
+        Controls.Add(btnDP)
+        Controls.Add(btnC)
+        Controls.Add(btn2)
+        Controls.Add(btn8)
+        Controls.Add(btn5)
+        Controls.Add(btnPM)
+        Controls.Add(btn0)
+        Controls.Add(btn1)
+        Controls.Add(btn7)
+        Controls.Add(btn4)
+        Controls.Add(btnBS)
+        Font = New Font("Sylfaen", 9.0F, FontStyle.Regular, GraphicsUnit.Point)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnBS As Button
+    Friend WithEvents btnPM As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btnP As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btnM As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnT As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnD As Button
+    Friend WithEvents btnE As Button
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents btnCE As Button
+    Friend WithEvents btnDP As Button
+    Friend WithEvents btn0 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+
+    Dim Firstvalue, Secondvalue, result As Double
+    Dim op As String
+
+    Private Sub Number_Click(sender As Object, e As EventArgs) Handles btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click, btn1.Click, btn2.Click, btn3.Click, btn0.Click, btnDP.Click
+        Dim b As Button = sender
+
+        If (txtDisplay.Text) = "0" Then
+            txtDisplay.Text = ""
+            txtDisplay.Text = b.Text
+
+        ElseIf b.Text = "." Then
+            If (Not txtDisplay.Text.Contains(".")) Then
+                txtDisplay.Text = txtDisplay.Text + b.Text
+
+            End If
+        Else
+            txtDisplay.Text = txtDisplay.Text + b.Text
+        End If
+    End Sub
+
+    Private Sub btnBS_Click(sender As Object, e As EventArgs) Handles btnBS.Click
+        If txtDisplay.Text.Length > 0 Then
+            txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1)
+        End If
+
+        If txtDisplay.Text.Length = "" Then
+            txtDisplay.Text = "0"
+
+        End If
+
+    End Sub
+
+    Private Sub Operator_Click(sender As Object, e As EventArgs) Handles btnP.Click, btnM.Click, btnT.Click, btnD.Click
+
+        Dim b As Button = sender
+        Firstvalue = txtDisplay.Text
+        op = b.Text
+        txtDisplay.Text = ""
+
+    End Sub
+
+    Private Sub btnE_Click(sender As Object, e As EventArgs) Handles btnE.Click
+
+        Secondvalue = txtDisplay.Text
+
+        If op = "+" Then
+
+            result = Firstvalue + Secondvalue
+            txtDisplay.Text = result
+
+        ElseIf op = "-" Then
+
+            result = Firstvalue - Secondvalue
+            txtDisplay.Text = result
+
+        ElseIf op = "×" Then
+
+            result = Firstvalue * Secondvalue
+            txtDisplay.Text = result
+
+        ElseIf op = "÷" Then
+
+            result = Firstvalue / Secondvalue
+            txtDisplay.Text = result
+
+        End If
+
+    End Sub
+
+    Private Sub btnPM_Click(sender As Object, e As EventArgs) Handles btnPM.Click
+
+        If (txtDisplay.Text.Contains("-")) Then
+            txtDisplay.Text = txtDisplay.Text.Remove(0, 1)
+
+        Else
+            txtDisplay.Text = "-" + txtDisplay.Text
+
+        End If
+
+    End Sub
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+
+        txtDisplay.Text = "0"
+
+    End Sub
+
+    Private Sub btnCE_Click(sender As Object, e As EventArgs) Handles btnCE.Click
+
+        txtDisplay.Text = "0"
+
+    End Sub
+End Class
